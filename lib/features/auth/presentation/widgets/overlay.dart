930619e893225cdebd030ad63f586ab5c6d3f77b
sithijatharuka <sithijatharuka03@gmail.com>
2025-08-10 00:43:51 +0530

    fix: Adjust overlay opacity for improved visibility

--- lib/features/auth/presentation/widgets/overlay.dart
+++ lib/features/auth/presentation/widgets/overlay.dart
@@ -12,10 +12,10 @@ class CustomOverlay extends StatelessWidget {
           end: Alignment.bottomCenter,
           colors: [
             Colors.black.withOpacity(
-              0.6,
+              0.5,
             ), // Increased opacity for darker overlay
             Colors.black.withOpacity(
-              0.7,
+              0.5,
             ), // Increased opacity for darker overlay
           ],
         ),
